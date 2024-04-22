65c6947b33638ddab975f8c8c145a9bb5a46a01a
asteriskzie <ester.gracia.10@gmail.com>
2024-04-22 18:43:06 +0700

    Add initial project files and configurations

---
+++ src/test/java/snackscription/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package snackscription.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
