7d5698d1d7c71ef9c19ec29f6fc29cd6b3e54f50
asteriskzie <ester.gracia.10@gmail.com>
2024-05-01 12:02:10 +0700

    [GREEN] Implement repository find by subscription box id and state

--- src/main/java/snackscription/review/repository/ReviewRepository.java
+++ src/main/java/snackscription/review/repository/ReviewRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import snackscription.review.model.Review;
+import snackscription.review.model.ReviewState;
 
 public interface ReviewRepository extends JpaRepository<Review, String> {
     List<Review> findBySubscriptionBoxId(String subsboxId);
+    List<Review> findBySubscriptionBoxIdAndState(String subsboxId, ReviewState state); 
     Review findBySubscriptionBoxIdAndUserId(String subsboxId, String userId);
     void deleteBySubscriptionBoxIdAndUserId(String subsboxId, String userId);
 }
